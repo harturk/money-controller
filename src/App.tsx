c69e3d7ad4c8dc788cc8ca08813b790207748b25
Hártur Kunzler Mainardi <hartur.kunzler@gmail.com>
2021-10-30 15:26:05 -0300

    Configuração front tela principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+
+/* <h1 className="title">Hello World</h1> 
+
+.title {
+    font-size: 64px;
+    color: #8257E6;
+};
+
+*/
+/*const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`; */
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
