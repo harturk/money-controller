72d77201204a6a98abb6ab03410f6b4d1a15c45d
Hártur Kunzler Mainardi <hartur.kunzler@gmail.com>
2021-11-01 20:39:05 -0300

    Interação com modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,21 +1,30 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { FormEvent, useState } from "react";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
-{
-  /* A propriedade isOpen recebe como estado inicial fechado ou "false" */
-}
+{/* A propriedade isOpen recebe como estado inicial fechado ou "false" */}
 export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
+  const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    // preventDefault impede que a página seja recarregada após cadastrar uma transação ("submitar")
+    event.preventDefault();
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -30,25 +39,45 @@ export function NewTransactionModal({
       >
         <img src={closeImg} alt="Fechar modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
-        <input placeholder="Título" />
-        <input type="number" placeholder="Valor" />
+        <input 
+          placeholder="Título" 
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+          />
+        <input 
+          type="number" 
+          placeholder="Valor" 
+          value={value}
+          /*event.target.value sempre retornará uma string, mesmo se o tipo for number
+          logo, precisará de conversão {event => setValue(+event.target.value)} */
+          onChange={event => setValue(Number(event.target.value))} 
+          />
         
         <TransactionTypeContainer>
-            <button 
-            type="button">
+            <RadioBox
+            type="button"
+            isActive={type === 'deposit'}
+            activeColor="green"
+            onClick={() => { setType('deposit'); }}>
                 <img src={incomeImg} alt="Entrada" />
                 <span>Entrada</span>
-            </button>
-            <button 
-            type="button">
+            </RadioBox>
+            <RadioBox 
+            type="button"
+            isActive={type === 'withdraw'}
+            activeColor="red"
+            onClick={() => { setType('withdraw'); }}>
                 <img src={outcomeImg} alt="Saída" />
                 <span>Saída</span>
-            </button>
+            </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input 
+          placeholder="Categoria" 
+          value={category}
+          onChange={event => setCategory(event.target.value)}/>
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
